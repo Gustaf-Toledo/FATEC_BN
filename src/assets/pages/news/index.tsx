d5fc453ff4358796dfa30b5604a5a1129a627d8a
Gustaf Toledo <gustta00@icloud.com>
2020-08-20 14:47:21 -0300

    Update index.tsx

--- src/assets/pages/news/index.tsx
+++ src/assets/pages/news/index.tsx
@@ -20,7 +20,7 @@ export default function NewsPage() {
                             <p>O site da FATEC Guarulhos está de cara nova!!</p>
                             <p>Nosso antigo portal infelizmente se corrompeu e ficou uma BAGUNÇA. Então os alunos do ADS decidiram fazer algo a respeito!</p>
                             <p>Construído utilizando React e TypeScript, o novo site é totalmente responsivo, ou seja, funciona tão bem em celulares quanto em computadores.
-                            Foi desenvolvido pelo aluno Gustaf (ou Gustavo) Toledo e sua companheira Eloize Rodrigues Xavier com ajuda dos colegas Vitor Santos e Ricardo Gomes, e juntamente o apoio do Coordenador do Curso, Milton Brito. </p>
+                            Foi desenvolvido pelo aluno Gustaf (ou Gustavo) Toledo e com ajuda dos colegas de classe Vitor Santos e Ricardo Gomes, e juntamente o apoio do Coordenador do Curso, Milton Brito e também o auxílio de Design de Eloize Rodrigues Xavier.</p>
                             <p>Segue as imagens de antes e depois!</p>
                             <div className="images">
                                 <img src={oldSite} alt="Antes"/>
